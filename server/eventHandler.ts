1246d1a11f55de81dc1850ff5997f72cbfc30607
Grace Tveit <grace.tveit@gmail.com>
2022-04-11 12:27:48 -0700

    feat: changing server to listen through http instead of https (temp)

--- server/eventHandler.ts
+++ server/eventHandler.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import https from 'https';
+import http from 'http';
+import 'dotenv/config';
+import eventsub from './eventsub';
 
 const app = express();
 
-const PORT = 443;
+app.use(
+  express.raw({
+    type: 'application/json',
+  })
+);
 
-https.createServer(app).listen(PORT, () => {
-  console.log(`listening at https://localhost:${PORT}`);
+app.post('/eventsub', eventsub);
+
+const PORT = 8080;
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
 });
